0ea75a6d1e91efc5332f5cf307c13efb652521bf
ashusatp <ashutoshworldking1234@gmail.com>
2025-10-19 03:27:26 +0530

    Update environment configuration, add key generation script, and enhance AuthController with JWT handling. created a script for generating RSA keys, and implemented access and refresh token generation in AuthController using the new keys.

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -1,8 +1,13 @@
+import fs from 'fs'
+import path from 'path'
 import { RegisterUserRequest } from '../types'
 import { Response, NextFunction } from 'express'
 import { UserService } from '../service/UserService'
 import { Logger } from 'winston'
 import { validationResult } from 'express-validator'
+import { JwtPayload, sign } from 'jsonwebtoken'
+import createHttpError from 'http-errors'
+import { Config } from '../config'
 
 export class AuthController {
     constructor(
@@ -40,6 +45,66 @@ export class AuthController {
             this.logger.info('User registered successfully', {
                 userId: user.id,
                 email: user.email,
+            })
+
+            let privateKey: Buffer
+            try {
+                privateKey = fs.readFileSync(
+                    path.join(__dirname, '../../certs/private.pem'),
+                )
+            } catch (error) {
+                const err = createHttpError(
+                    500,
+                    'Failed to read private key from file',
+                    {
+                        cause:
+                            error instanceof Error
+                                ? error.message
+                                : 'Unknown error',
+                    },
+                )
+                next(err)
+                return
+            }
+
+            const patload: JwtPayload = {
+                sub: String(user.id),
+                role: user.role,
+            }
+
+            const accessToken = sign(patload, privateKey, {
+                algorithm: 'RS256',
+                expiresIn: '1h',
+                issuer: 'auth-service',
+            })
+
+            if (!Config.REFRESH_JWT_SECRET) {
+                const err = createHttpError(
+                    500,
+                    'Refresh JWT secret not configured',
+                )
+                next(err)
+                return
+            }
+
+            const refreshToken = sign(patload, Config.REFRESH_JWT_SECRET, {
+                algorithm: 'HS256',
+                expiresIn: '30d',
+                issuer: 'auth-service',
+            })
+
+            // set cookies
+            res.cookie('access_token', accessToken, {
+                domain: 'localhost',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 60, // 1 hour
+                httpOnly: true, // very important
+            })
+            res.cookie('refresh_token', refreshToken, {
+                domain: 'localhost',
+                sameSite: 'strict',
+                maxAge: 1000 * 60 * 60 * 24 * 30, // 30 days
+                httpOnly: true, // very important
             })
 
             // response
