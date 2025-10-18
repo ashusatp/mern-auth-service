88b4212954c51600cbfd4cbc8142a316fddda23f
ashusatp <ashutoshworldking1234@gmail.com>
2025-10-19 02:07:26 +0530

    Enhance registration input validation by adding trimming and length checks for password, first name, and last name fields. Updated email validation to include trimming and conversion to lowercase.

--- src/validators/register-validators.ts
+++ src/validators/register-validators.ts
@@ -4,11 +4,26 @@ export default [
     body('email')
         .notEmpty()
         .withMessage('Email is required')
+        .trim()
         .isEmail()
-        .withMessage('Invalid email'),
-    body('password').notEmpty().withMessage('Password is required'),
-    body('firstName').notEmpty().withMessage('First name is required'),
-    body('lastName').notEmpty().withMessage('Last name is required'),
+        .withMessage('Invalid email')
+        .toLowerCase(),
+
+    body('password')
+        .notEmpty()
+        .withMessage('Password is required')
+        .trim()
+        .isLength({ min: 8 })
+        .withMessage('Password must be at least 8 characters long'),
+
+    body('firstName')
+        .notEmpty()
+        .withMessage('First name is required')
+        .trim()
+        .isLength({ min: 3 })
+        .withMessage('First name must be at least 3 characters long'),
+
+    body('lastName').notEmpty().withMessage('Last name is required').trim(),
 ]
 
 // import { checkSchema } from 'express-validator'
